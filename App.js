be27b409a4d4db699a482b2cb9bc5d5d9f979d69
UnCompa <brandonddxd@gmail.com>
2024-12-03 22:31:11 -0500

    refactot ♻️  cambio de nombres

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 
       <Stack.Navigator>
         <Stack.Screen name='WelcomBrandon' component={Home} />
-        <Stack.Screen name='ContactsNav' component={Contacts} />
+        <Stack.Screen name='CallNumber' component={Contacts} />
         <Stack.Screen name='ProductNavigation' component={Product}/>
 
 
