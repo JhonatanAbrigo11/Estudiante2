93e752f0de86273c0873241e344a103cdf22d644
UnCompa <brandonddxd@gmail.com>
2024-12-03 22:05:58 -0500

    feat 🎇  cambios en products.js
    Se agrego textos con nombre y tres productos

--- app/screens/Product.js
+++ app/screens/Product.js
@@ -1,14 +1,17 @@
-import {View, Text, StyleSheet, Button} from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export const Product = ({navigation}) =>{
     return <View style={styles.container}> 
-        <Text>Estas en productos</Text>
+        <Text style={{fontWeight: "900",color: "white"}}>Bienvenido a mi tienda, soy Brandon</Text>
         <Button
             title='HOME'
             onPress={()=>{
                 navigation.navigate('HomeNav');
             }}
         />
+        <Text style={{fontWeight: "100",color: "yellow"}}>Tomate</Text>
+        <Text style={{fontWeight: "500",color: "red"}}>Lechuja</Text>
+        <Text style={{fontWeight: "bold", color: "green"}}>Naranjas</Text>
     </View>
 
 }
@@ -16,7 +19,7 @@ export const Product = ({navigation}) =>{
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: '#212121',
       alignItems: 'center',
       justifyContent: 'center',
     },
